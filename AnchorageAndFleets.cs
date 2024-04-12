b30a423b7d7e3aa4b0993a77d6f58ffa7aee73aa
Samuel Berre <qiz015@uib.no>
2024-04-12 19:16:20 +0200

    Attempting to solve merge mistake...

---
+++ AnchorageAndFleets.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+
+// Classes used to create objects for fleets and anchorages
+
+public class Fleet
+{
+    public required Dimensions singleShipDimensions { get; set; }
+    public required string shipDesignation { get; set; }
+    public required int shipCount { get; set; }
+}
+
+public class Dimensions
+{
+    public required int width { get; set; }
+    public required int height { get; set; }
+}
+
+public class AnchorageAndFleets
+    // Contains everything needed to store and manipulate data on fleets and anchorages.
+{
+    public required Dimensions anchorageSize { get; set; }
+    public required List<Fleet> fleets { get; set; }
+
+    int[,] anchorage;
+    int[,] tempAnchorage;
+    public ArrayList finalAnchorageList = new ArrayList();
+    int shipNumber = 1; // used for visual representation of ships
+
+    public ArrayList RunAlgorithm()
+    // Runs through algorithm, returning a list of all anchorages with ships
+    {
+        while (MoreShipsRemaining())
+        {
+            while (AddShipToAnchorageIfPossible())
+            { UpdateShipNumber(); };
+            GoToNextAnchorage();
+        }
+        return finalAnchorageList;
+    }
+
+    public void UpdateShipNumber()
+    // Updates ship number, used to visualize where different ships are on anchorage
+    {
+        if (++shipNumber > 9)
+            shipNumber = 1;
+    }
+
+    public void SortFleets()
+    // Sorts list of fleets by their longest side, from shortest to longest
+    {
+        fleets.Sort((y, x) => (
+            Math.Max(x.singleShipDimensions.width, x.singleShipDimensions.height).CompareTo(
+                Math.Max(y.singleShipDimensions.width, y.singleShipDimensions.height))
+            ));
+    }
+
+    public void InitializeNewAnchorage()
+    // Sets variable anchorage to empty anchorage with correct dimensions
+    {
+        anchorage = new int[anchorageSize.height, anchorageSize.width];
+    }
+
+    public void GoToNextAnchorage()
+    // Saves anchorage and starts a new iteration
+    {
+        finalAnchorageList.Add(anchorage);
+        InitializeNewAnchorage();
+    }
+
+    public bool MoreShipsRemaining()
+    // Returns true if there are more ships of any type, false if all ships are placed
+    {
+        foreach (Fleet fleet in fleets)
+        {
+            if (fleet.shipCount > 0)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool AddShipToAnchorageIfPossible()
+    // Goes through all ships and tries to place one. Returns false if anchorage is full
+    {
+        foreach (Fleet currentFleet in fleets)
+        {
+            if (currentFleet.shipCount > 0)
+            {
+                if (TryToPlaceShipAllLocations(currentFleet.singleShipDimensions.width, currentFleet.singleShipDimensions.height))
+                {
+                    currentFleet.shipCount -= 1;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public bool TryToPlaceShipAllLocations(int shipWidth, int shipHeight)
+    // Systematically goes through available spots on anchorage, left to right, then top to bottom, and tries to place ship there
+    {
+        for (int y = 0; y < anchorage.GetLength(0); y++)
+        {
+            for (int x = 0; x < anchorage.GetLength(1); x++)
+            {
+                if (anchorage[y, x] == 0)
+                {
+                    if (TryToPlaceShipAtCoordinates(y, x, shipWidth, shipHeight))
+                        return true;
+                    else if (TryToPlaceShipAtCoordinates(y, x, shipHeight, shipWidth)) // Try to place ship tilted 90 degrees
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public bool TryToPlaceShipAtCoordinates(int anchorageY, int anchorageX, int shipWidth, int shipHeight)
+    // Tries to place ship on specified anchorage coordinates
+    {
+        // if ship does not go out of anchorage bounds
+        if (anchorageY + shipWidth <= anchorage.GetLength(0) && anchorageX + shipHeight <= anchorage.GetLength(1))
+        {
+            tempAnchorage = (int[,])anchorage.Clone();
+            for (int y = anchorageY; y < anchorageY + shipWidth; y++)
+            {
+                for (int x = anchorageX; x < anchorageX + shipHeight; x++)
+                {
+                    if (tempAnchorage[y, x] == 0)
+                        tempAnchorage[y, x] = shipNumber;
+                    else
+                        return false;
+                }
+            }
+            anchorage = (int[,])tempAnchorage.Clone(); // Place ship if there is sufficient space
+            return true;
+        }
+        return false;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -23,37 +23,14 @@ if (app.Environment.IsDevelopment())
 
 ///
 
-// Code to get list of ships, anchorage
 var httpClient = new HttpClient();
-//string randomFleetsAndAnchorage; // JSON format
-AnchorageAndFleets anchorageAndFleets;
-int[,] anchorage;
-int[,] tempAnchorage;
-ArrayList finalAnchorageList = new ArrayList();
-int shipNumber = 1; // used for visual representation of ships
 
 string runAlgorithmFromPost(AnchorageAndFleets request)
     // Takes POST data, calculated optimal anchorage layout, sends back solution
 {
-    anchorageAndFleets = request;
-    tempAnchorage = new int[anchorageAndFleets.anchorageSize.height, anchorageAndFleets.anchorageSize.width];
-    finalAnchorageList = new ArrayList();
-    InitializeNewAnchorage();
-    ArrayList test = RunAlgorithm();
-    Console.WriteLine(test[0]);
+    request.InitializeNewAnchorage();
+    ArrayList test = request.RunAlgorithm();
     return PrintFittedAnchorageInfo(test);
-}
-
-ArrayList RunAlgorithm()
-// Runs through algorithm, returning a list of all anchorages with ships
-{
-    while (MoreShipsRemaining())
-    {
-        while (AddShipToAnchorageIfPossible())
-        { UpdateShipNumber(); };
-        GoToNextAnchorage();
-    }
-    return finalAnchorageList;
 }
 
 async Task<bool> InitializeRandomFleetsAsync()
@@ -70,21 +47,21 @@ async Task<bool> InitializeRandomFleetsAsync()
             string responseAsString = await httpResponse.Content.ReadAsStringAsync();
 
             // Why do I get a warning here ??
-            anchorageAndFleets = JsonSerializer.Deserialize<AnchorageAndFleets>(responseAsString) ??
+            AnchorageAndFleets anchorageAndFleets = JsonSerializer.Deserialize<AnchorageAndFleets>(responseAsString) ??
             throw new InvalidOperationException();
 
-            SortFleets();
+            anchorageAndFleets.SortFleets();
 
             foreach (Fleet fleet in anchorageAndFleets.fleets)
             {
                 Console.WriteLine(fleet.singleShipDimensions.width + " " + fleet.singleShipDimensions.height);
             }
 
-            InitializeNewAnchorage();
+            anchorageAndFleets.InitializeNewAnchorage();
 
-            RunAlgorithm();
+            anchorageAndFleets.RunAlgorithm();
 
-            PrintFittedAnchorageInfo(finalAnchorageList);
+            PrintFittedAnchorageInfo(anchorageAndFleets.finalAnchorageList);
 
             return true;
 
@@ -131,132 +108,7 @@ string PrintFittedAnchorageInfo(ArrayList anchorage)
     return finalString;
 }
 
-void SortFleets()
-// Sorts list of fleets by their longest side, from shortest to longest
-{
-    anchorageAndFleets.fleets.Sort((y, x) => (
-        Math.Max(x.singleShipDimensions.width, x.singleShipDimensions.height).CompareTo(
-            Math.Max(y.singleShipDimensions.width, y.singleShipDimensions.height))
-        ));
-}
-
-void InitializeNewAnchorage()
-// Sets variable anchorage to empty anchorage with correct dimensions
-{
-    anchorage = new int[anchorageAndFleets.anchorageSize.height, anchorageAndFleets.anchorageSize.width];
-}
-
-void GoToNextAnchorage()
-// Saves anchorage and starts a new iteration
-{
-    finalAnchorageList.Add(anchorage);
-    InitializeNewAnchorage();
-}
-
-bool MoreShipsRemaining()
-// Returns true if there are more ships of any type, false if all ships are placed
-{
-    foreach (Fleet fleet in anchorageAndFleets.fleets)
-    {
-        if (fleet.shipCount > 0)
-        {
-            return true;
-        }
-    }
-    return false;
-}
-
-bool AddShipToAnchorageIfPossible()
-// Goes through all ships and tries to place one. Returns false if anchorage is full
-{
-    foreach (Fleet currentFleet in anchorageAndFleets.fleets)
-    {
-        if (currentFleet.shipCount > 0)
-        {
-            if (TryToPlaceShipAllLocations(currentFleet.singleShipDimensions.width, currentFleet.singleShipDimensions.height))
-            {
-                currentFleet.shipCount -= 1;
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-bool TryToPlaceShipAllLocations(int shipWidth, int shipHeight)
-// Systematically goes through available spots on anchorage, left to right, then top to bottom, and tries to place ship there
-{
-    for (int y = 0; y < anchorage.GetLength(0); y++)
-    {
-        for (int x = 0; x < anchorage.GetLength(1); x++)
-        {
-            if (anchorage[y, x] == 0)
-            {
-                if (TryToPlaceShipAtCoordinates(y, x, shipWidth, shipHeight))
-                    return true;
-                else if (TryToPlaceShipAtCoordinates(y, x, shipHeight, shipWidth)) // Try to place ship tilted 90 degrees
-                    return true;
-            }
-        }
-    }
-    return false;
-}
-
-bool TryToPlaceShipAtCoordinates(int anchorageY, int anchorageX, int shipWidth, int shipHeight)
-// Tries to place ship on specified anchorage coordinates
-{
-    // if ship does not go out of anchorage bounds
-    if (anchorageY + shipWidth <= anchorage.GetLength(0) && anchorageX + shipHeight <= anchorage.GetLength(1))
-    {
-        tempAnchorage = (int[,])anchorage.Clone();
-        for (int y = anchorageY; y < anchorageY + shipWidth; y++)
-        {
-            for (int x = anchorageX; x < anchorageX + shipHeight; x++)
-            {
-                if (tempAnchorage[y, x] == 0)
-                    tempAnchorage[y, x] = shipNumber;
-                else
-                    return false;
-            }
-        }
-        anchorage = (int[,])tempAnchorage.Clone(); // Place ship if there is sufficient space
-        return true;
-    }
-    return false;
-}
-
-void UpdateShipNumber()
-// Updates ship number, used to visualize where different ships are on anchorage
-{
-    if (++shipNumber > 9)
-        shipNumber = 1;
-}
-
 app.MapPost("/GetOptimalAnchorages", (AnchorageAndFleets request) => runAlgorithmFromPost(request)
 );
 
 app.Run();
-
-// Return list 
-
-// Classes used to create objects for fleets and anchorage
-public class Fleet
-{
-    public required Dimensions singleShipDimensions { get; set; }
-    public required string shipDesignation { get; set; }
-    public required int shipCount { get; set; }
-}
-
-public class Dimensions
-{
-    public required int width { get; set; }
-    public required int height { get; set; }
-}
-
-public class AnchorageAndFleets
-{
-    public required Dimensions anchorageSize { get; set; }
-    public required List<Fleet> fleets { get; set; }
-}
-
-///
